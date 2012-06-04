66b189c6d265f410bd5c7657962f741430589020
Felipe Casadei <felipe.casadei@gmail.com>
2012-06-03 21:52:50 -0300

    Added accelerometer support.

--- assets/js/mindthegap.js
+++ assets/js/mindthegap.js
@@ -1,13 +1,60 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
+var accelerometerWatcher = null;
+
+$(document).ready(function () {
+    bindButtons();
+});
+
 function onDeviceReady() {
-    loadDevice();
+    bindButtons();
+    deviceReady();
 }
 
-function loadDevice() {
-    document.getElementById('device-name').innerHTML = device.name;
-    document.getElementById('device-cordova').innerHTML = device.cordova;
-    document.getElementById('device-platform').innerHTML = device.platform;
-    document.getElementById('device-uuid').innerHTML = device.uuid;
-    document.getElementById('device-verrsion').innerHTML = device.version;
+function bindButtons() {
+    $('#accelerometer-watch').bind("change", function(event, ui) { 
+        if ($('#accelerometer-watch').is(':checked')) {
+            startAccelerometerWatcher();
+        } else {
+            stopAccelerometerWatcher();
+        }
+    });
+
+    $('#accelerometer-get').bind("click", getAccelerometer);
+}
+
+function deviceReady() {
+    $('#device-name').innerHTML = device.name;
+    $('#device-cordova').innerHTML = device.cordova;
+    $('#device-platform').innerHTML = device.platform;
+    $('#device-uuid').innerHTML = device.uuid;
+    $('#device-verrsion').innerHTML = device.version;
+}
+
+function getAccelerometer() {
+    navigator.accelerometer.getCurrentAcceleration(accelerometerSuccess, accelerometerError);
+}
+
+function startAccelerometerWatcher() {
+    if (!accelerometerWatcher) {
+        accelerometerWatcher = navigator.accelerometer.watchAcceleration(accelerometerSuccess, accelerometerError, { frequency: 5000 });
+    }
+}
+
+function stopAccelerometerWatcher() {
+    if (accelerometerWatcher) {
+        navigator.accelerometer.clearWatch(accelerometerWatcher);
+        accelerometerWatcher = null;        
+    }
+}
+
+function accelerometerSuccess(acceleration) {
+    $('#accelerometer-x').innerHTML = acceleration.x;
+    $('#accelerometer-y').innerHTML = acceleration.y;
+    $('#accelerometer-z').innerHTML = acceleration.z;
+    $('#accelerometer-timestamp').innerHTML = acceleration.timestamp;
+}
+
+function accelerometerError() {
+    alert('Erro ao tentar obter as informações do aceler&ocirc;metro.');
 }
